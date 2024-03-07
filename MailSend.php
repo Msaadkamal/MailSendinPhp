9bbca879801b98ca1482a278d97679866f196735
Muhammad Saad Kamal <Msaadkamal1999@gmail.com>
2024-03-08 03:22:56 +0500

    Update on github

---
+++ MailSend.php
@@ -0,0 +1,18 @@
+<?php
+
+$to= "M";
+$subject="Test mail";
+$message="This  is simple email message";
+$from=""
+$header="From : $from";
+mail($to ,$subject, $message,$header);
+echo"Mail send";
+
+
+
+
+
+
+
+
+?>
