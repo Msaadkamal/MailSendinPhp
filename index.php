4fd894dc4726c42ceda629c879ac967c9017c5f4
Muhammad Saad Kamal <Msaadkamal1999@gmail.com>
2024-03-08 04:51:23 +0500

    Add some changis

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-$to= "M";
+$to= "msaadkamal1999@gmail.com";
 $subject="Test mail";
 $message="This  is simple email message";
-$from=""
+$from="ammarmughal698@gmail.com"
 $header="From : $from";
 mail($to ,$subject, $message,$header);
 echo"Mail send";
